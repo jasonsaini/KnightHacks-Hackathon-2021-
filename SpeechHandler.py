3ad30f5027bbaff59677052344d0a9b74a925e3d
jasonsaini <jasonsaini.exe@gmail.com>
2021-11-13 11:27:22 -0500

    Implemented speech recognition & text-to-speech functionality

--- SpeechHandler.py
+++ SpeechHandler.py
@@ -1,29 +1,41 @@
 import speech_recognition as sr
 import pyttsx3
 
-class SpeechHandler:
-    #initalize text-to-speech engine
-    ttsEngine = pyttsx3.init()
-    
-    def __init__():
-        # Set voice to male
-        voices = ttsEngine,getProperty('voices')
-        ttsEngine.setProperty('voice', voices[0].id)
+#initalize text-to-speech engine
+ttsEngine = pyttsx3.init()
+#initalize speech-to-text engine
+recognizer = sr.Recognizer()
 
-        """ 
-        SET VOLUME
-        volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
-        engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
-        
-        
-        SET RATE   
-        rate = engine.getProperty('rate')   # getting details of current speaking rate
-        engine.setProperty('rate', 125)     # setting up new voice rate
-        
-        """
-    
+# Set voice to male
+voices = ttsEngine.getProperty('voices')
+ttsEngine.setProperty('voice', voices[0].id)
+
+# set volume
+volume = ttsEngine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
+ttsEngine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
+
+# set rate   
+rate = ttsEngine.getProperty('rate')   # getting details of current speaking rate
+ttsEngine.setProperty('rate', 165)     # setting up new voice rate
+
+class SpeechHandler:
+   
+   # translates any text passed in to audio (speaking)
     def speak(message):
-        engine.say(message)
-        engine.runAndWait()
-        engine.stop()
+        ttsEngine.say(message)
+        ttsEngine.runAndWait()
+        ttsEngine.stop()
         return
+
+    def listen():
+        #print("Say something...")
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source, duration = 0.2)
+            audio = recognizer.listen(source)
+            # return transcribed audio as string
+        try:
+            return recognizer.recognize_google(audio)
+        except sr.UnknownValueError:
+            return ""
+        except sr.RequestError:
+            return ""
