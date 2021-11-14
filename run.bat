b2f47bc46591c39f24fa187bb2158fe73c3c9ca6
jasonsaini <jasonsaini.exe@gmail.com>
2021-11-14 09:49:00 -0500

    Added dependency installation via batch file

--- run.bat
+++ run.bat
@@ -1,4 +1,10 @@
 @ECHO OFF
 ECHO Running DadBot 2.0...
+pip install playsound
+pip install PyQt5
+pip install speechRecognition
+pip install pyttsx3
+pip install dadjokes
+pip install pyjokes
 python -u "%~dp0\main.py %*"
 PAUSE
